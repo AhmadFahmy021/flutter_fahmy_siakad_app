0a3abbd39138a5604eb893483f225bd5151a4514
ahmadfahmy021 <ahmadfahmy219@gmail.com>
2023-10-22 08:58:18 +0700

    First Application Flutter

---
+++ android/app/src/main/kotlin/com/example/flutter_fahmy_siakad_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_fahmy_siakad_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/common/constants/variabels.dart
@@ -0,0 +1,3 @@
+class Variables {
+  static const String baseUrl = 'https://9639-123-253-233-0.ngrok-free.app';
+}
